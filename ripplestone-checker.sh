f3a57818766617653a4d231a3adaed169fad5da3
Mark Nouri <mjnouri@gmail.com>
2019-12-13 10:29:49 -0500

    added OpenSSH to Windows box to repair service

--- ripplestone-checker.sh
+++ ripplestone-checker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Amazon Linux 2
-
 # remember to add the following to /etc/crontab to run this every 5 minutes
 # */5 * * * * ec2-user /home/ec2-user/mark/website-checker/ripplestone-checker.sh
 
@@ -11,21 +9,24 @@
 # check user mail, which shows the resulting output of the commands from cron
 # sudo tail -30 /var/mail/root
 
-# Give EC2 SNS permission
-
 
 DATEANDTIME=$(date +%F" "%r" "%A)
 UPMESSAGE=$"Ripplestone is up."
-DOWNMESSAGE=$"Ripplestone is down."
+DOWNMESSAGE=$"Ripplestone was down and was reset."
 
-curl -I "https://www.google.com" > curl-result
 
-if grep -q 200 curl-result
+curl -I "www.google.com" > /home/ec2-user/mark/website-checker/curl-result
+
+
+if grep -q 200 /home/ec2-user/mark/website-checker/curl-result
 then
  echo $UPMESSAGE
- echo "$DATEANDTIME - $UPMESSAGE" >> log
+ echo "$DATEANDTIME - $UPMESSAGE" >> /home/ec2-user/mark/website-checker/log
+ echo "-------------------------" >> /home/ec2-user/mark/website-checker/log
 else
  echo $DOWNMESSAGE
- echo "$DATEANDTIME - $DOWNMESSAGE" >> log
- aws sns publish --topic-arn arn:aws:sns:us-east-1:123456:Me --message "$DOWNMESSAGE"
+ echo "$DATEANDTIME - $DOWNMESSAGE" >> /home/ec2-user/mark/website-checker/log
+ aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$DOWNMESSAGE"
+ ssh windowsUser@172.31.104.231 'C:\Scripts\RestartIIS-RunAsAdmin.bat' >> /home/ec2-user/mark/website-checker/log
+ echo "-------------------------" >> /home/ec2-user/mark/website-checker/log
 fi
