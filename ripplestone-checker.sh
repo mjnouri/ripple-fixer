05af77c85ec42086a44bc0f633906ab4a9502a92
Mark Nouri <mjnouri@gmail.com>
2019-12-23 21:22:16 +0000

    added a few more features, and broke it for now

--- ripplestone-checker.sh
+++ ripplestone-checker.sh
@@ -9,22 +9,43 @@
 # check user mail, which shows the resulting output of the commands from cron
 # sudo tail -30 /var/mail/root
 
-
 DATEANDTIME=$(date +%F" "%r" "%A)
 UPMESSAGE=$"Ripplestone is up."
 DOWNMESSAGE=$"Ripplestone was down and was reset."
+RIPPLEHOST=172.31.46.121
 
+echo $DATEANDTIME | tee -a /home/centos/website-checker/log
+echo "" | tee -a log
 
-curl -I "www.google.com" > /home/ec2-user/mark/website-checker/curl-result
+nc -z $RIPPLEHOST 22
+if [[ $? -eq 0 ]]
+then
+  echo "Ripplestone server is reachable on SSH." | tee -a /home/centos/website-checker/log
+else
+  echo "Ripplestone server is unreachable on SSH. Check status of server." | tee -a /home/centos/website-checker/log
+  echo "" | tee -a /home/centos/website-checker/log
+  echo "--------------------------------------" | tee -a /home/centos/website-checker/log
+  exit 1
+fi
 
+curl -I "www.google.com" | tee /home/centos/website-checker/curl-result
 
-if grep -q 200 /home/ec2-user/mark/website-checker/curl-result
+if grep -q 200 /home/centos/website-checker/curl-result
 then
  echo $UPMESSAGE
+ echo $UPMESSAGE | tee -a /home/centos/website-checker/log
 else
  echo $DOWNMESSAGE
- echo "$DATEANDTIME - $DOWNMESSAGE" >> /home/ec2-user/mark/website-checker/log
- aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$DOWNMESSAGE" >> /home/ec2-user/mark/website-checker/log
- ssh windowsUser@172.31.104.231 'C:\Scripts\RestartIIS-RunAsAdmin.bat' >> /home/ec2-user/mark/website-checker/log
- echo "----------------------------------------------" >> /home/ec2-user/mark/website-checker/log
+ echo $DOWNMESSAGE | tee -a /home/centos/website-checker/log
+ aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$DOWNMESSAGE" | tee -a /home/centos/website-checker/log
+ ssh windowsUser@$RIPPLEHOST 'C:\Scripts\restart-spooler.bat' | tee -a /home/centos/website-checker/log | tee /home/centos/website-checker/output
+ echo "----------------------------------------------" | tee -a /home/centos/website-checker/log
 fi
+
+if grep -q "The Print Spooler service was started successfully." /home/centos/website-checker/curl-result
+then
+ echo "Message from script - Print Spooler was restarted" | tee -a /home/centos/website-checker/log
+else
+ echo "Message from script - Print Spooler was not restarted. Check the server" | tee -a /home/centos/website-checker/log
+fi
+
