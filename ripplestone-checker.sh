a41f533146789f4e2b7436f75107d12a34cdd3bd
Mark Nouri <mjnouri@gmail.com>
2019-12-25 11:03:45 -0500

    removed unnecessary tee usage

--- ripplestone-checker.sh
+++ ripplestone-checker.sh
@@ -18,19 +18,16 @@ RIPPLEDOWN=$"Ripplestone is down."
 RIPPLERESTARTED=$"Ripplestone was restarted."
 RIPPLENOTRESTARTED=$"Ripplestone was NOT restarted. Check the service."
 
-echo $DATEANDTIME | tee -a /home/centos/website-checker/log
-echo "" | tee -a log
+echo $DATEANDTIME >> /home/centos/website-checker/log
 
 nc -z $RIPPLEHOST 22
 if [[ $? -eq 0 ]]
 then
-  echo "Ripplestone server is reachable on SSH." | tee -a /home/centos/website-checker/log
-  echo "" | tee -a /home/centos/website-checker/log
+  echo "Ripplestone server is reachable on SSH." >> /home/centos/website-checker/log
 else
-  echo "Ripplestone server is unreachable on SSH. Check status of server." | tee -a /home/centos/website-checker/log
-  aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$SERVERUNAVAILABLE" | tee -a /home/centos/website-checker/log
-  echo "" | tee -a /home/centos/website-checker/log
-  echo "--------------------------------------" | tee -a /home/centos/website-checker/log
+  echo "Ripplestone server is unreachable on SSH. Check status of server." >> /home/centos/website-checker/log
+  aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$SERVERUNAVAILABLE" >> /home/centos/website-checker/log
+  echo "--------------------------------------" >> /home/centos/website-checker/log
   exit 1
 fi
 
@@ -39,23 +36,20 @@ curl -I "google.com" | tee /home/centos/website-checker/curl-result 1> /dev/null
 
 if grep -q "HTTP/1.1 200" /home/centos/website-checker/curl-result
 then
-  echo $UPMESSAGE | tee -a /home/centos/website-checker/log
+  echo $UPMESSAGE >> /home/centos/website-checker/log
   exit 0
 else
-  echo $RIPPLEDOWN | tee -a /home/centos/website-checker/log
+  echo $RIPPLEDOWN >> /home/centos/website-checker/log
   ssh mnouri@$RIPPLEHOST 'C:\Scripts\restart-ripplestone.bat' | tee -a /home/centos/website-checker/log | tee /home/centos/website-checker/bat-script-output
 fi
 
 if grep -q "The Print Spooler service was started successfully." /home/centos/website-checker/bat-script-output
 then
-  echo $RIPPLERESTARTED | tee -a /home/centos/website-checker/log
-  aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$RIPPLERESTARTED" | tee -a /home/centos/website-checker/log
-  echo "--------------------------------------" | tee -a /home/centos/website-checker/log
+  echo $RIPPLERESTARTED >> /home/centos/website-checker/log
+  aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$RIPPLERESTARTED" >> /home/centos/website-checker/log
   exit 0
 else
-  echo $RIPPLENOTRESTARTED | tee -a /home/centos/website-checker/log
-  aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$RIPPLENOTRESTARTED" | tee -a /home/centos/website-checker/log
-  echo "--------------------------------------" | tee -a /home/centos/website-checker/log
+  echo $RIPPLENOTRESTARTED >> /home/centos/website-checker/log
+  aws sns publish --topic-arn arn:aws:sns:us-east-1:123:Me --message "$RIPPLENOTRESTARTED" >> /home/centos/website-checker/log
   exit 1
 fi
-
